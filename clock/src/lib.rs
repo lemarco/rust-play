3896b57fc9a83c41f94bb31739092cc53589be24
Lemarc <lemarc.dev@gmail.com>
2023-10-01 12:37:39 +0200

    fix clock task

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,36 +1,30 @@
 use std::fmt::Display;
-
+const DAY: i32 = 24 * 60;
+const HOUR: i32 = 60;
 #[derive(PartialEq, Eq, Debug)]
 pub struct Clock {
-    hours: i32,
     minutes: i32,
 }
-
+fn normalize(minutes: i32) -> i32 {
+    ((minutes % DAY) + DAY) % DAY
+}
 impl Clock {
     pub fn new(hours: i32, minutes: i32) -> Self {
-        Self { hours, minutes }
+        Self {
+            minutes: normalize(hours * HOUR + minutes),
+        }
     }
 
     pub fn add_minutes(&self, minutes: i32) -> Self {
         Self {
-            hours: self.hours,
-            minutes: self.minutes + minutes,
+            minutes: normalize(self.minutes + minutes),
         }
     }
 }
 impl Display for Clock {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let minutes = if self.minutes > 10 {
-            format!("{}", self.minutes)
-        } else {
-            format!("0{}", self.minutes)
-        };
-        let hours = if self.hours > 10 {
-            format!("{}", self.hours)
-        } else {
-            format!("0{}", self.hours)
-        };
-
-        write!(f, "{}:{}", hours, minutes)
+        let minutes = self.minutes % HOUR;
+        let hours = self.minutes / HOUR;
+        write!(f, "{:02}:{:02}", hours, minutes)
     }
 }
